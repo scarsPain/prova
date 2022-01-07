f2ed1d412d38586d7d80d91f848d8a745ad15e42
scarsPain <34248798+scarsPain@users.noreply.github.com>
2022-01-07 10:22:06 +0100

    start up

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+d
+4src/main/java/com/example/prova/HelloController.java,3/1/313d03699b3a5e15744a1f16396d6705150d6e01
+e
+5src/main/java/com/example/prova/HelloApplication.java,d/9/d92d6af57a91cc727ef9f67fa7207cd4b24a6289
+9
+	prova.iml,6/b/6b5bb607a8b3453f6e65a197e4b0efa55db64dbd
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.prova {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.prova to javafx.fxml;
+    exports com.example.prova;
+}
